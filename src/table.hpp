380c5dc5d407cb22e38817098203cda0f55cf59c
coldTOUFU <coldtoufu.dev@gmail.com>
2022-08-30 13:51:24 +0900

    change: Table構造体でサーバから通知されるすべての情報を保持するよう修正。

--- src/table.hpp
+++ src/table.hpp
@@ -7,21 +7,30 @@ namespace uecda {
   struct Table {
     Table(uecda::common::CommunicationBody src) {
       this->is_my_turn = src[5][2];
+      this->whose_turn = src[5][3];
       this->is_start_of_trick = src[5][4];
       this->is_lock = src[5][7];
       this->is_rev = src[5][6];
-      for (int i = 0; i < 4; i++) {
-        this->is_out[i] = src[6][i] == 0;
+      for (int i = 0; i < 5; i++) {
+	this->card_quantity_of_players[i] = src[6][i];
+        this->is_out[i] = this->card_quantity_of_players[i] == 0;
+	this->rank_of_players[i] = src[6][5 + i];
+	this->player_num_on_seats[i] = src[6][10 + i];
       }
     }
 
-    bool is_my_turn;
-    bool is_start_of_trick;
-    bool is_lock;
-    bool is_rev;
-    bool has_passed[4]; // 各プレイヤがパスをしたか。
-    bool is_out[4]; // 各プレイヤがあがったか。
+    bool is_my_turn;                 // 自分のターンか？
+    int whose_turn;                  // 誰のターンか(プレイヤ番号)。
+    bool is_start_of_trick;          // 場に何もないか？
+    bool is_lock;                    // 縛り中？
+    bool is_rev;                     // 革命中？
+    bool has_passed[5];              // 各プレイヤがパスをしたか(ユーザが自分で更新するメンバ)。
+    int card_quantity_of_players[5]; // 各プレイヤのカード枚数。
+    bool is_out[5];                  // 各プレイヤがあがったか。
+    int rank_of_players[5];          // 各プレイヤの現階級。
+    int player_num_on_seats[5];      // 各席のプレイヤの番号。
   };
 }
 
 #endif // TABLE_HPP_
+

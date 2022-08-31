f92247d801dc02a73510e123a4124e251d5e8d65
coldTOUFU <coldtoufu.dev@gmail.com>
2022-08-31 19:31:13 +0900

    change: テストがすべて通るようにした。

--- src/sample_client/select_hand.cpp
+++ src/sample_client/select_hand.cpp
@@ -11,6 +11,7 @@ std::vector<Hand> select_change_hands(std::vector<Hand> &hands) {
     }
   }
 
+  /* 強さの昇順に整列。交換枚数がnの場合、0からn個とればいいようにする。 */
   sort(single_without_joker.begin(), single_without_joker.end(), [](Hand &h1, Hand &h2) {
     return h1.getSummary().strongest_order > h2.getSummary().strongest_order;
   });
@@ -18,7 +19,7 @@ std::vector<Hand> select_change_hands(std::vector<Hand> &hands) {
   return single_without_joker;
 }
 
-/* 手の枚数と強さの観点のみから最善の手を選ぶ。 */
+/* 同じ型の手の中から、手の枚数と強さの観点のみから最善の手を選ぶ。handsには1つ以上の合法手が存在すると仮定。 */
 Hand &select_best_hand_in_same_hand_type(std::vector<Hand> &hands, bool is_rev) {
   Hand &best_hand = hands.at(0);
   for (Hand &hand : hands) {
@@ -40,52 +41,6 @@ Hand &select_best_hand_in_same_hand_type(std::vector<Hand> &hands, bool is_rev) 
   return best_hand;
 }
 
-/* 1枚組を出す場合、複数枚役を崩さないモノを優先する。 */
-Hand select_isolated_hand(Cards my_cards, std::vector<Hand>& hands, const Hand& table_hand, const Table& table) {
-  /* 手の候補がないかカードが空なら(あり得ないが)、パスの手を返す。 */
-  if (my_cards.quantity() <= 0 || hands.size() <= 0) {
-    return Hand();
-  }
-
-  /* ジョーカーなしで複数枚組を構成するカードを候補から除外。 */
-  for (const Hand &hand : hands) {
-    const HandSummary summary = hand.getSummary();
-     if(!summary.has_joker) {
-      if (summary.quantity > 1) {
-        my_cards -= hand.getCards();
-      }
-    }
-  }
-
-  /* 候補のカードが0になったらパスを返す。 */
-  if (my_cards.quantity() <= 0) {
-    return Hand();
-  }
-
-  /* 候補の役を生成。 */
-  std::vector<Hand> isolated_hands;  
-  Hand::pushHands(my_cards, isolated_hands);
-
-  /* 候補からジョーカー以外の合法な1枚出しを取り出す。 */
-  std::vector<Hand> single_without_joker;
-  for (const Hand &hand : hands) {
-    if (!hand.isLegal(table, table_hand)) {
-      continue;
-    }
-    const HandSummary summary = hand.getSummary();
-    if (!summary.has_joker && summary.quantity == 1) {
-      single_without_joker.push_back(hand);
-    }
-  }
-
-  /* 最良の手を返す。 */
-  if (single_without_joker.size() > 0) {
-    return select_best_hand_in_same_hand_type(single_without_joker, table.is_rev);
-  } else {
-    return Hand();
-  }
-}
-
 Hand select_hand(std::vector<Hand> &hands, const Hand &table_hand, const Table &table) {
   /* 手の候補がなければ(あり得ないが)、パスの手を返す。 */
   if (hands.size() <= 0) {
@@ -99,13 +54,50 @@ Hand select_hand(std::vector<Hand> &hands, const Hand &table_hand, const Table &
   std::vector<Hand> pair_with_joker(0);
   std::vector<Hand> sequence_with_joker(0);
 
+  /* 枚数が多い手に含まれるカードを考慮から除外することで、枚数の多い手を崩さないようにする。 */
+  Cards pair_excluded_cards = Cards({});
+  Cards sequence_excluded_cards = Cards({});
+
+  /* 枚数の多い手を先に確認して*_excluded_cardsに加えるために、枚数の多い順に並び替える。 */
+  std::sort(hands.begin(), hands.end(), [](Hand a, Hand b) {
+    return a.getSummary().quantity > b.getSummary().quantity;
+  });
+
   /* 着手候補を手の種類・ジョーカーの有無で別々の配列に分ける。 */
   for (const Hand &hand : hands) {
+    const HandSummary summary = hand.getSummary();
+
+    /* 出せる手の枚数が確定しているなら、より枚数の多い手に含まれるカードを出さないようにする。 */
+    if (!table.is_start_of_trick && !summary.has_joker && summary.quantity > table_hand.getSummary().quantity) {
+      if (summary.is_sequence) {
+        sequence_excluded_cards += hand.getCards();
+      } else {
+        pair_excluded_cards += hand.getCards();
+      }
+    }
+
+    /* 非合法なら出さない。 */
     if (!hand.isLegal(table, table_hand)) {
       continue;
     }
 
-    const HandSummary summary = hand.getSummary();
+    /* *_excluded_cardsに含まれるカードがあるなら、手を出さない。 */
+    /* ただし、(オリジナルのdefaultでそうしているので、)縛り中は気にしない。 */
+    if (!table.is_lock && summary.is_sequence &&
+        sequence_excluded_cards.filterCards(hand.getCards().getCard()) != (Cards::bitcards)0) {
+      continue;
+    } else if (!table.is_lock &&
+               pair_excluded_cards.filterCards(hand.getCards().getCard()) != (Cards::bitcards)0) {
+      continue;
+    }
+    /* 1枚出しの場合は、縛りがあってもペアや階段を崩さないようにする。 */
+    if (summary.quantity == 1 &&
+        (sequence_excluded_cards.filterCards(hand.getCards().getCard()) != (Cards::bitcards)0 ||
+        pair_excluded_cards.filterCards(hand.getCards().getCard()) != (Cards::bitcards)0)) {
+      continue;
+    }
+
+    /* 手の性質に合わせてそれぞれの配列に挿入する。 */
     if (!summary.has_joker) {
       if (summary.quantity == 1) {
         single_without_joker.push_back(hand);
@@ -125,29 +117,45 @@ Hand select_hand(std::vector<Hand> &hands, const Hand &table_hand, const Table &
     }
   }
 
-  /* 階段→n(>1)枚出し→1枚出し、ジョーカーなし→ジョーカーありの順で手を探索 */
-  if (sequence_without_joker.size() > 0) {
-    return select_best_hand_in_same_hand_type(sequence_without_joker, table.is_rev);
-  }
+  /* 場にカードがある場合: 階段→n(>1)枚出し→1枚出し、ジョーカーなし→ジョーカーありの順で手を探索 */
+  /* 場にカードがない場合: 階段→n(>1)枚出し→1枚出しの順で手を探索(ジョーカー区別なし) */
+  if (!table.is_start_of_trick) {
+    if (sequence_without_joker.size() > 0) {
+      return select_best_hand_in_same_hand_type(sequence_without_joker, table.is_rev);
+    }
 
-  if (sequence_with_joker.size() > 0) {
-    return select_best_hand_in_same_hand_type(sequence_with_joker, table.is_rev);
-  }
+    if (sequence_with_joker.size() > 0) {
+      return select_best_hand_in_same_hand_type(sequence_with_joker, table.is_rev);
+    }
 
-  if (pair_without_joker.size() > 0) {
-    return select_best_hand_in_same_hand_type(pair_without_joker, table.is_rev);
-  }
+    if (pair_without_joker.size() > 0) {
+      return select_best_hand_in_same_hand_type(pair_without_joker, table.is_rev);
+    }
 
-  if (pair_with_joker.size() > 0) {
-    return select_best_hand_in_same_hand_type(pair_with_joker, table.is_rev);
-  }
+    if (pair_with_joker.size() > 0) {
+      return select_best_hand_in_same_hand_type(pair_with_joker, table.is_rev);
+    }
 
-  if (single_without_joker.size() > 0) {
-    return select_best_hand_in_same_hand_type(single_without_joker, table.is_rev);
-  }
+    if (single_without_joker.size() > 0) {
+      return select_best_hand_in_same_hand_type(single_without_joker, table.is_rev);
+    }
 
-  if (single_with_joker.size() > 0) {
-    return select_best_hand_in_same_hand_type(single_with_joker, table.is_rev);
+    if (single_with_joker.size() > 0) {
+      return select_best_hand_in_same_hand_type(single_with_joker, table.is_rev);
+    }
+  } else {
+    sequence_without_joker.insert(sequence_without_joker.end(), sequence_with_joker.begin(), sequence_with_joker.end());
+    pair_without_joker.insert(pair_without_joker.end(), pair_with_joker.begin(), pair_with_joker.end());
+    single_without_joker.insert(single_without_joker.end(), single_with_joker.begin(), single_with_joker.end());
+    if (sequence_without_joker.size() > 0) {
+      return select_best_hand_in_same_hand_type(sequence_without_joker, table.is_rev);
+    }
+    if (pair_without_joker.size() > 0) {
+      return select_best_hand_in_same_hand_type(pair_without_joker, table.is_rev);
+    }
+    if (single_without_joker.size() > 0) {
+      return select_best_hand_in_same_hand_type(single_without_joker, table.is_rev);
+    }
   }
 
   /* 合法手がなかった場合はパスを返す。 */
--- src/sample_client/select_hand.hpp
+++ src/sample_client/select_hand.hpp
@@ -11,7 +11,6 @@ using namespace uecda;
 
 std::vector<Hand> select_change_hands(std::vector<Hand>& hands);
 Hand &select_best_hand_in_same_hand_type(std::vector<Hand> &hands, bool is_rev);
-Hand select_isolated_hand(Cards my_cards, std::vector<Hand>& hands, const Hand& table_hand, const Table& table);
 Hand select_hand(std::vector<Hand>& hands, const Hand& table_hand, const Table& table);
 
 #endif // SELECT_HAND_HPP_
